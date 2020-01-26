37449bd08494bc4f32150e60eecbabe8e264d848
Nagisa Day <nuggit.o@gmail.com>
2020-01-26 05:27:22 -0800

    Bump version and update export script

--- rtexport.sh
+++ rtexport.sh
@@ -7,10 +7,12 @@ mkdir rtzip
 mkdir rtzip/color-thief
 mkdir rtzip/color-thief/src
 
-cp rainbow-tabs/icon* rtzip
+cp rainbow-tabs/Rainbow*.png rtzip
 cp rainbow-tabs/background.js rtzip
 cp rainbow-tabs/manifest.json rtzip
 cp rainbow-tabs/color-thief/src/color-thief.js rtzip/color-thief/src
 
 zip -r rainbow-tabs.zip rtzip
 rm -r rtzip
+
+echo "Go to https://chrome.google.com/webstore/developer/dashboard and click Edit on Rainbow Tabs"
